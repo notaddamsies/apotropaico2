d8d5cad23d32c9c4499b0d9ad6c9b8e88545c2fe
Adam Storai <storaiadam@gmail.com>
2023-04-19 17:37:16 +0200

    ssss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CategoriesComponent } from './features/categories/categories.component';
+import { PostsComponent } from './features/posts/posts.component';
+import { CategoryFormComponent } from './features/category-form/category-form.component';
+import { PostFormComponent } from './features/post-form/post-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    PostsComponent,
+    CategoryFormComponent,
+    PostFormComponent
   ],
   imports: [
     BrowserModule,
